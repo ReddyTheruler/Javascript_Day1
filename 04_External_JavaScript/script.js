d6737d0c0c37ab3a5bcfbdd74e1a307617350b36
ReddyTheRuler <jani.reddy5@gmail.com>
2018-09-10 22:57:27 +0530

    Hello

---
+++ 04_External_JavaScript/script.js
@@ -0,0 +1,8 @@
+ function blue() {
+        document.getElementById('green-p').style.color = 'blue';
+    }
+    function red() {
+        document.getElementById('green-p').style.color = 'red';
+    }
+    
+   
